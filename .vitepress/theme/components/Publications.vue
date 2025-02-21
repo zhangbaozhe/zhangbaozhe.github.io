5c06c2998186e1cc67e2f2946ed97356e776aba8
Baozhe Zhang <zhang.baozhe2000@gmail.com>
2025-02-21 13:12:16 +0800

    updated for mobile

--- .vitepress/theme/components/Publications.vue
+++ .vitepress/theme/components/Publications.vue
@@ -7,6 +7,8 @@ onMounted(async () => {
   const response = await fetch("/publications.json"); // Adjust path if necessary
   publications.value = await response.json();
 });
+
+
 </script>
 
 <template>
@@ -17,14 +19,14 @@ onMounted(async () => {
         <a :href="pub.link" target="_blank">
           <strong>{{ pub.title }}</strong>
         </a>
-        <p style="font-size: small; margin: auto;">
+        <p class="author-list">
           <span v-for="(author, idx) in pub.authors" :key="idx">
             <span :class="{ 'bold-author': author.includes('Baozhe Zhang') }">{{ author }}</span>
             <span v-if="idx < pub.authors.length - 1">, </span>
           </span>
         </p>
-        <p style="font-size: small; margin: auto;"><em>{{ pub.venue }}</em></p>
-        <p style="font-size: small; margin: auto;">{{ pub.excerpt }}</p>
+        <p class="venue"><em>{{ pub.venue }}</em></p>
+        <p class="excerpt">{{ pub.excerpt }}</p>
       </div>
     </div>
   </div>
@@ -32,29 +34,113 @@ onMounted(async () => {
 
 <style scoped>
 .publications {
-  padding: 5px;
+  padding: 0px;
 }
 
 .publication-item {
   display: flex;
+  flex-direction: row;
   gap: 15px;
-  align-items: center;
+  align-items: flex-start;
   margin-bottom: 20px;
-
-  padding: 10px;
+  padding: 0px;
   border-radius: 8px;
+  width: 100%;
+  box-sizing: border-box;
 }
 
 .publication-image {
   width: 200px;
+  height: auto;
   border-radius: 5px;
+  object-fit: cover;
+  margin: auto;
 }
 
 .publication-details {
   flex: 1;
+  min-width: 200px;
 }
 
 .bold-author {
   font-weight: bold;
+}
+
+.author-list,
+.venue,
+.excerpt {
+  font-size: 1rem;
+  margin: auto;
+}
+
+.author-list {
+  word-wrap: break-word;
+  overflow-wrap: break-word;
+  text-overflow: ellipsis;
+}
+
+.venue {
+  font-style: italic;
+}
+
+.excerpt {
+  word-wrap: break-word;
+}
+
+
+
+@media (max-width: 768px) {
+  .publication-item {
+    flex-direction: column;
+    align-items: flex-start;
+    padding: 0px;
+  }
+
+  .publication-image {
+    width: 100%;
+    max-width: 150px;
+    margin-bottom: 5px;
+  }
+
+  .publication-details {
+    text-align: left;
+  }
+
+  .author-list {
+    word-wrap: break-word;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+  }
+
+  .venue {
+    font-size: 0.9rem;
+  }
+
+  .excerpt {
+    font-size: 0.9rem;
+  }
+
+  .author-list,
+  .excerpt {
+    display: none;
+    /* Hide the author and excerpt sections by default on mobile */
+  }
+
+}
+
+@media (max-width: 480px) {
+  .publication-item {
+    padding: 0px;
+  }
+
+  .publication-details {
+    font-size: 0.9rem;
+  }
+
+  .author-list,
+  .venue,
+  .excerpt {
+    font-size: 0.85rem;
+  }
 }
 </style>
